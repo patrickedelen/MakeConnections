06ec70a05e0567996c7ec00324229827fc5ce922
Patrick <patrickedelen@gmail.com>
2016-03-22 08:56:50 -0500

    Started the rendering development

--- MakeConnections/src/makeconnections/MakeConnections.java
+++ MakeConnections/src/makeconnections/MakeConnections.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package makeconnections;
+import java.awt.*;
+import javax.swing.JComponent;
 
 /**
  *
@@ -14,8 +16,33 @@ public class MakeConnections {
     /**
      * @param args the command line arguments
      */
+    
+    public FindConnections fc = new FindConnections();
+    
     public static void main(String[] args) {
-        System.out.println("Hi");
+        //initiate the frame and draw the game board
+        
+        drawBoard();
+    }
+    
+    public static void drawBoard(){
+        //create the canvas
+        Canvas canv1 = new Canvas();
+        canv1.setSize(800,800);
+        canv1.setBackground(Color.red);
+        
+        Frame frame1 = new Frame();
+        frame1.add(canv1);
+        frame1.setLayout(new FlowLayout());
+        frame1.setSize(800,800);
+        frame1.setVisible(true);
+        
+        //set the mouse to the game board
+        //requestFocus();
+    }
+    
+    public void paint(Graphics g){
+        
     }
     
 }
