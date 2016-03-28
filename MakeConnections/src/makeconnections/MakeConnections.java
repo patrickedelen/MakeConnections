9bc2e7409dce7888b1e213d3732b5d66e3971d44
Patrick <patrickedelen@gmail.com>
2016-03-27 23:28:36 -0500

    Game prints out current board position to console, chip dropping implemented

--- MakeConnections/src/makeconnections/MakeConnections.java
+++ MakeConnections/src/makeconnections/MakeConnections.java
@@ -7,6 +7,9 @@ package makeconnections;
 import java.awt.*;
 import javax.swing.JComponent;
 import javax.swing.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -19,13 +22,39 @@ public class MakeConnections extends JPanel{
      */
     
     public FindConnections fc = new FindConnections();
+    public static Scanner scan = new Scanner(System.in);
+    
+    //create the connect 4 game board with 7 columns and 6 rows
+    public static int[][] board = new int[6][7]; 
     
     public static void main(String[] args) {
         //initiate the frame and draw the game board
         
-        
+
+        boolean running = true;
         
         drawBoard();
+        System.out.println("Welcome to MakeConnections!");
+        System.out.println("Use the numbers 0-6 to play a chip");
+        System.out.println("First to four wins!\n");
+        
+        //print the game board
+        printBoard();
+        
+        while(running){
+            //get player1 choice
+            int column = scan.nextInt();
+            scan.nextLine();
+            dropChip(1, column);
+            
+            //get player2 choice
+            column = scan.nextInt();
+            scan.nextLine();
+            dropChip(2, column);
+            
+            //loop
+        }
+        
     }
     
     public static void drawBoard(){
@@ -44,6 +73,31 @@ public class MakeConnections extends JPanel{
         //requestFocus();
         
     }
+    
+    public static void printBoard(){
+        System.out.println("0 1 2 3 4 5 6");
+        System.out.println("-------------");
+        for(int a = 0; a < 6; a++){
+            for(int b = 0; b < 7; b++){
+                System.out.print(board[a][b] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void dropChip(int player, int column){
+        
+        for(int r = 5; r >= 0; r--){
+            if(board[r][column] == 0){
+                board[r][column] = player;
+                r = 0;
+            }
+        }
+        
+        printBoard();
+    
+    }
+    
     private boolean updateBoard() {
         repaint();
         return true;
