a360bdee68856c17fa377f70833b9a421810e47c
Patrick <patrickedelen@gmail.com>
2016-03-31 08:19:23 -0500

    Added horizontal connection checking

--- MakeConnections/src/makeconnections/MakeConnections.java
+++ MakeConnections/src/makeconnections/MakeConnections.java
@@ -63,7 +63,7 @@ public class MakeConnections extends JPanel{
         frame1.setLayout(new FlowLayout());
         frame1.setSize(800,800);
         frame1.setLocationRelativeTo(null);
-        frame1.setVisible(true);
+        //frame1.setVisible(true);
         //set the mouse to the game board
         //requestFocus();
         
@@ -81,7 +81,7 @@ public class MakeConnections extends JPanel{
     }
     
     public static void dropChip(boolean player1, int column){
-        
+        //Checking the height of the board and setting the first empty spot in the collumn to the player's number
         for(int r = 5; r >= 0; r--){
             if(board[r][column] == 0){
                 board[r][column] = (player1) ? 1 : 2;
@@ -96,19 +96,31 @@ public class MakeConnections extends JPanel{
     
     public static void checkBoard(boolean player1){
         int player = player1 ? 1 : 2;
+        //System.out.println("player: " + player);
         
-        //check vertical chips
-        for(int a = 0; a < 6; a++){
-            for(int b = 5; b > 2; b--){
-                if(!(board[a][b] == 0)){
+        //check vertical chips starting with a (vertical) 0
+        for(int a = 5; a > 2; a--){
+            for(int b = 0; b < 7; b++){
+                if(board[a][b] == player){
+                    System.out.println("checking vertical board position: " + a + " " + b);
                     checkVertical(a, b, 0, player);
+                }
+            }
+        }
+        
+        //check horizontal chips starting with a (vertical) 0
+        for(int a = 5; a >= 0; a--){
+            for(int b = 0; b < 4; b++){
+                if(board[a][b] == player){
+                    System.out.println("checking horizontal board position: " + a + " " + b);
+                    checkHorizontal(a, b, 0, player);
                 }
             }
         }
     }
     
     public static void checkVertical(int a, int b, int count, int player){
-        if(a < 6 && count < 4){
+        if(a >= 0){
             if(board[a][b] == player){
                 count++;
                 
@@ -117,12 +129,31 @@ public class MakeConnections extends JPanel{
                     System.out.println("Connection made! Player " + board[a][b] + " wins!");
                     running = false;
                 } else {
-                    a++;
+                    a--;
                     checkVertical(a, b, count, player); 
                 }
             }
-        } else if(a == 6){
+        } else if(a < 0){
             System.out.println("No connection made " + b);
+        }
+    }
+    
+    public static void checkHorizontal(int a, int b, int count, int player){
+        if(b < 7){
+            if(board[a][b] == player){
+                count++;
+                
+                if(count == 4){
+                    printBoard();
+                    System.out.println("Connection made! Player " + board[a][b] + " wins!");
+                    running = false;
+                } else {
+                    b++;
+                    checkHorizontal(a, b, count, player);
+                }
+            }
+        } else if(b > 7){
+            System.out.println("No horizontal connection made " + a);
         }
     }
     
@@ -131,6 +162,7 @@ public class MakeConnections extends JPanel{
         return true;
     }
     
+    @Override
     public void paint(Graphics g){
         g.setColor(Color.WHITE);
         g.drawOval(5, 5, 10, 10);
